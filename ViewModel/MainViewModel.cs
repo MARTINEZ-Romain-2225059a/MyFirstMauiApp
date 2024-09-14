6c2615818dd581d96fb1165335724c5b55d86a4f
Xpery <martinezromain622@gmail.com>
2024-09-14 13:27:25 +0200

    MainViewModel update

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -1,11 +1,33 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 
 namespace MyFirstMauiApp.ViewModel
 {
     public partial class MainViewModel : ObservableObject
     {
+
+        public MainViewModel()
+        {
+            Items = new ObservableCollection<string>();
+        }
+
+        [ObservableProperty]
+        ObservableCollection<string> items;
+
         [ObservableProperty]
         string text;
+
+        [RelayCommand]
+        void Add()
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                return;
+
+            Items.Add(Text);
+            //add our item
+            Text = string.Empty;
+        }
     }
 }

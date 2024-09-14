b6a971fa16ecbc37bba8e772df9dd8f3fa7a3739
Xpery <martinezromain622@gmail.com>
2024-09-14 13:09:11 +0200

    Création de MainViewModel

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,11 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.ComponentModel;
+
+namespace MyFirstMauiApp.ViewModel
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        string text;
+    }
+}
